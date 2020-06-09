8687a759a179a1a376fee6911c8d3bf0cbd78795
Matheus Muriel <matheus.muriel@outlook.com>
2020-06-08 23:46:55 -0300

    SVG

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,31 @@
-import { hot } from "react-hot-loader/root";
 import * as React from "react";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import FooView from "./views/FooView";
-import BarView from "./views/BarView";
+import { hot } from "react-hot-loader/root";
+
 import Forca from "./views/Forca";
+
 import "./styles/main.scss";
 
+//<Forca></Forca>
 function App() {
   return (
-    <Forca></Forca>
+    <div className="container">
+
+      <div className="forca-container">
+        <Forca></Forca>
+      </div>
+
+      <div className="infos-container">
+        <div className="placar-container">
+          <div id="placar"></div>
+        </div>
+        <div className="palavra-container">
+          <h1>AA_A_AA_AAAA_AAA</h1>
+        </div>
+        <div className="letras-container">
+          <p>A B C D E F G H I J K L M N O P Q R S T U V W X Y Z</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- client/src/views/Forca.tsx
+++ client/src/views/Forca.tsx
@@ -1,36 +1,32 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
 
-export interface ILayoutProps {
-}
+import forca from './forca.svg'; // Tell webpack this JS file uses this image
 
 const Forca = () => {
 
-    const getTime = () => {
-        let now = new Date();
-        let year = now.getFullYear().toString();
-        let month = now.getMonth().toString().padStart(2, "0");
-        let day = now.getDate().toString().padStart(2, "0");
-        return [year, month, day].reduce((prev, curr) => prev + "-" + curr);
-    }
+  const getTime = () => {
+    let now = new Date();
+    let year = now.getFullYear().toString();
+    let month = now.getMonth().toString().padStart(2, "0");
+    let day = now.getDate().toString().padStart(2, "0");
+    return [year, month, day].reduce((prev, curr) => prev + "-" + curr);
+  }
 
-    return (
-        <div className="layout">
-            <div className="container">
-                <div className="sidebar">
-                    
-                </div>
-                <div className="header">
-                    <h1>My Application</h1>
-                </div>
-                <div className="footer">
-                    <p>Today is: {getTime()}</p>
-                </div>
-            </div>
+  return (
+    <div className="layout">
+      <img src={forca} alt="..." />
+      <div className="container">
+        <div className="sidebar">
         </div>
-    );
+        <div className="header">
+          <h1>My Application</h1>
+        </div>
+        <div className="footer">
+          <p>Today is: {getTime()}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-
-//export const Forca: React.FunctionComponent<ILayoutProps>
 export default Forca;

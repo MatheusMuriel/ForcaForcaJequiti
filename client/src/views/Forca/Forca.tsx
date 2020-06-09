839987ba2d2448210e0fc6a02cab302a8b71ddb8
Matheus Muriel <matheus.muriel@outlook.com>
2020-06-09 12:39:04 -0300

    Novos Assets

--- client/src/views/Forca/Forca.tsx
+++ client/src/views/Forca/Forca.tsx
@@ -1,27 +1,29 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { getEnforcamento } from "../../services/silvioSantos";
 
-import forca0 from '../../assets/forca_0.svg';
-import forca1 from '../../assets/forca_1.svg';
-import forca2 from '../../assets/forca_2.svg';
-import forca3 from '../../assets/forca_3.svg';
-import forca4 from '../../assets/forca_4.svg';
-import forca5 from '../../assets/forca_5.svg';
-import forca6 from '../../assets/forca_6.svg';
+import './style.scss';
+
+import forca0 from '../../assets/forca0.png';
+import forca1 from '../../assets/forca1.png';
+import forca2 from '../../assets/forca2.png';
+import forca3 from '../../assets/forca3.png';
+import forca4 from '../../assets/forca4.png';
+import forca5 from '../../assets/forca5.png';
+import forca6 from '../../assets/forca6.png';
+import forca7 from '../../assets/forca7.png';
 
 const Forca = () => {
-  const nivelForc = getEnforcamento();
+  const forcas = [forca0, forca1, forca2, forca3, forca4, forca5, forca6, forca7];
+  
+  const [srcForca, setSrcForca] = useState<string>(forca0);
+
+  useEffect(() => {
+    const nivel = getEnforcamento();
+    setSrcForca(forcas[nivel]);
+  }, [getEnforcamento()]);
 
   return (
-    <>
-      { (nivelForc === 0) && (<img src={forca0} alt="..." />)  }
-      { (nivelForc === 1) && (<img src={forca1} alt="..." />)  }
-      { (nivelForc === 2) && (<img src={forca2} alt="..." />)  }
-      { (nivelForc === 3) && (<img src={forca3} alt="..." />)  }
-      { (nivelForc === 4) && (<img src={forca4} alt="..." />)  }
-      { (nivelForc === 5) && (<img src={forca5} alt="..." />)  }
-      { (nivelForc === 6) && (<img src={forca6} alt="..." />)  }
-    </>
+    <img src={srcForca} alt="..." className="forca" />
   );
 }
 
